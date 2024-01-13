530e926dc0904cc07f56476bb6a071b22ad9533b
Felix Tripier <tripier@ionq.co>
2024-01-13 16:18:06 -0500

    Revert "try bitflip xor"
    This reverts commit 51d7209ec2199f4e24d5688d000eadd4dd4edf42.

--- harvard_sim/src/bit_matrix/mod.rs
+++ harvard_sim/src/bit_matrix/mod.rs
@@ -1,13 +1,1 @@
-use bitvec::prelude::*;
-
-pub struct BitMatrix {
-    rows: Vec<BitVec>,
-}
-
-impl BitMatrix {
-    pub fn zeroes(rows: usize, cols: usize) -> Self {
-        Self {
-            rows: (0..rows).map(|_| bitvec![usize, Lsb0; 0; cols ]).collect(),
-        }
-    }
-}
+pub struct BitMatrix {}
